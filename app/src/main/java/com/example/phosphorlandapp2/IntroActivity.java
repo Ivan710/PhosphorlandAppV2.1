6cc2c7c30580f7c8a324c570eeb8505ba2841567
Ivan710 <filipovicivan2005@gmail.com>
2022-10-13 12:54:17 +0200

    lol

--- app/src/main/java/com/example/phosphorlandapp2/IntroActivity.java
+++ app/src/main/java/com/example/phosphorlandapp2/IntroActivity.java
@@ -1,14 +1,25 @@
 package com.example.phosphorlandapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntroActivity extends AppCompatActivity {
-
+private ConstraintLayout start_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        start_btn=findViewById(R.id.start_btn);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
