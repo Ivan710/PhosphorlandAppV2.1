b814fbbca54de89454a0d7aac1463643b3b383a4
Ivan710 <filipovicivan2005@gmail.com>
2022-10-24 17:37:59 +0200

    lol

--- app/src/main/java/com/example/phosphorlandapp2/PickMirtiloActivity.java
+++ app/src/main/java/com/example/phosphorlandapp2/PickMirtiloActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ShowDetail2Activity extends AppCompatActivity {
+public class PickMirtiloActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_detail2);
+        setContentView(R.layout.activity_pick_mirtilo);
     }
 }
--- app/src/main/java/com/example/phosphorlandapp2/ShowDetailActivity.java
+++ app/src/main/java/com/example/phosphorlandapp2/ShowDetailActivity.java
@@ -7,19 +7,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ShowDetailActivity extends AppCompatActivity {
-private TextView addToCartBtn;
-private TextView feeTxt, numberOrderTxt;
-private ImageView plusBtn, minusBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_detail);
 
-        initVieW();
-    }
-
-    private void initVieW() {
-        addToCartBtn=findViewById(R.id.addToCartBtn);
-        feeTxt=findViewById(R.id.priceTxt);
     }
 }
