d7d2214e342dde0bebbd6d715133ed06afd58c5d
Ivan710 <filipovicivan2005@gmail.com>
2022-10-27 13:18:48 +0200

    lol

--- app/src/main/java/com/example/phosphorlandapp2/OrderOtherActivity.java
+++ app/src/main/java/com/example/phosphorlandapp2/OrderOtherActivity.java
@@ -1,20 +1,65 @@
 package com.example.phosphorlandapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class OrderOtherActivity extends AppCompatActivity {
     TextView textView;
+    private ConstraintLayout constraintLayout5555;
     public static String tekst;
+    int minteger = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_other);
+        constraintLayout5555=findViewById(R.id.ConstraintLayout);
+        constraintLayout5555.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OrderOtherActivity.this, OrderingActivity.class));
+            }
+        });
 
 
         textView=(TextView)findViewById(R.id.title);
         textView.setText(tekst);
+    }
+    public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
+
+    }public void decreaseInteger(View view) {
+        minteger = minteger - 1;
+        if (minteger <= 0)
+            minteger = 0;
+        display(minteger);
+    }
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.integer_number);
+        displayInteger.setText("" + number + " kg");
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
